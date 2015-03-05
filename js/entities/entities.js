01b36c9b84db26756a5187ae2d28e61df3557970
Roy Marquez <theroyal3655@gmail.com>
2015-03-05 14:04:38 -0800

    vid9

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -1,3 +1,40 @@
 game.PlayerEntity = me.Entity.extend({
-    init: function
-    });
+    init: function(x, y, settings) {
+        this._super(me.Entity, 'init', [x, y, {
+             //Sets the player model and space it takes up to a variable
+             image: "player",
+             width: 64,
+             height: 64,
+             spritewidth: "64",
+             spriteheight: "64",
+             getShape: function(){
+                 return(new me.Rect(0, 0, 64, 64)).toPolygon();
+             }
+        }]);
+    
+        this.body.setVelocity(5, 20);
+    
+    },
+    
+    update: function(delta) {
+        if(me.input.isKeyPressed("right")) {
+            //Adds to the position of my x by the velocity defined above in
+            //setVelocity() and multiplying it by me.timer.tick
+            //me.timer.tick makes the movement smooth
+           this.body.vel.x += this.body.accel.x * me.timer.tick; 
+        }else if(me.input.isKeyPressed("left")) {
+            this.body.vel.x -= this.body.accel.x * me.timer.tick;
+        }else{
+            this.body.vel.x = 0;
+        }
+        
+        this.body.update(delta);
+        return true;
+    
+       
+        
+        this.body.update(delta);
+        return true;
+      }
+      
+});
