5bb32b71c2bd7be7d43a350ba6516b494456746b
Roy Marquez <theroyal3655@gmail.com>
2015-04-22 13:23:19 -0700

    commitin

--- js/game.js
+++ js/game.js
@@ -6,11 +6,11 @@ var game = {
 	data : {
 		// score
 		score : 0,
-                enemyBaseHealth: 10,
-                playerBaseHealth: 10,
+                enemyBaseHealth: 1,
+                playerBaseHealth: 1,
                 enemyCreepHealth: 5,
-                playerHealth: 1000000,
-                enemyCreepAttack: 5,
+                playerHealth: 20,
+                enemyCreepAttack: 1,
                 playerAttack: 5,
 //                orcBaseDamage: 10,
 //                orcBaseHealth: 100,
@@ -50,6 +50,8 @@ var game = {
 	}
         
         me.save.add({exp: 0, exp1: 0, exp2: 0, exp3: 0, exp4: 0});
+        
+        me.state.SPENDEXP = 112;
 
 	// Initialize the audio.
 	me.audio.init("mp3,ogg");
@@ -76,6 +78,7 @@ var game = {
                 
 		me.state.set(me.state.MENU, new game.TitleScreen());
 		me.state.set(me.state.PLAY, new game.PlayScreen());
+                me.state.set(me.state.SPENDEXP, new game.SpendExp());
 
 		// Start the game.
 		me.state.change(me.state.MENU);
--- js/screens/title.js
+++ js/screens/title.js
@@ -8,21 +8,57 @@ game.TitleScreen = me.ScreenObject.extend({
                 
                 me.game.world.addChild(new (me.Renderable.extend({
                    init: function(){
-                       this._super(me.Renderable, 'init', [10, 30, me.game.viewport.width, me.game.viewport.height]);
+                       this._super(me.Renderable, 'init', [270, 240, 300, 50]);
                        this.font = new me.Font("Arial", 46, "white");
+                       me.input.registerPointerEvent("pointerdown", this, this.newGame.bind(this), true);
                    },
                    
                    draw: function(renderer){
-                     this.font.draw(renderer.getContext(), "Awesomenauts!", 450, 130);
-                     this.font.draw(renderer.getContext(), "Press ENTER to play!", 250, 530);
+                     this.font.draw(renderer.getContext(), "Start a New Game", 400, this.pos.x, this.pos.y); 
+                   },
+                   
+                   update: function(dt){
+                      return true; 
+                   },
+                   
+                   newGame: function(){
+                      me.input.releasePointerEvent("pointerdown", this);
+                      me.save.remove("exp");
+                      me.save.remove("exp1");
+                      me.save.remove("exp2");
+                      me.save.remove("exp3");
+                      me.save.remove("exp4");
+                      me.state.change(me.state.PLAY);
                    }
                 })));
                 
-                this.handler = me.event.subscribe(me.event.KEYDOWN, function(action, keyCode, edge){
-                    if(action === "start"){
-                        me.state.change(me.state.PLAY);
-                    }
-                });
+              me.game.world.addChild(new (me.Renderable.extend({
+                   init: function(){
+                       this._super(me.Renderable, 'init', [380, 340, 250, 50]);
+                       this.font = new me.Font("Arial", 46, "white");
+                       me.input.registerPointerEvent("pointerdown", this, this.newGame.bind(this), true);
+                   },
+                   
+                   draw: function(renderer){
+                     this.font.draw(renderer.getContext(), "Continue", 400, this.pos.x, this.pos.y); 
+                   },
+                   
+                   update: function(dt){
+                      return true; 
+                   },
+                   
+                   newGame: function(){
+                       game.data.exp = me.save.exp;
+                       game.data.exp1 = me.save.exp1;
+                       game.data.exp2 = me.save.exp2;
+                       game.data.exp3 = me.save.exp3;
+                       game.data.exp4 = me.save.exp4;
+                      me.input.releasePointerEvent("pointerdown", this);
+                      me.state.change(me.state.PLAY);
+                   }
+                })));   
+                
+               
         
         },
 	
@@ -31,7 +67,6 @@ game.TitleScreen = me.ScreenObject.extend({
 	 *  action to perform when leaving this screen (state change)
 	 */
 	onDestroyEvent: function() {
-		me.input.unbindKey(me.input.KEY.ENTER); // TODO
-                me.event.unsubscribe(this.handler);
+		
         }
 });
